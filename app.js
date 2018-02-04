bc304c641e7717d491008ef14473fea4957c4643
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-02-04 14:52:15 -0800

    utilize fs.readfile

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const http = require('http');
+const fs = require('fs');
 
-const hostName = 'localhost';
+const host = 'localhost';
 const port = 3000;
 
-const server = http.createServer(function(req, res) {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Test');
+fs.readFile('index.html', (err, data) => {
+    
+    if (err) throw err;
+
+    const server = http.createServer((req, res) => {
+        
+        if (req.url === '/') {
+            res.writeHead(200, { 'Content-type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+    });
+
+    server.listen(port, host, () => {
+        console.log(`Now listening to: ${host} ${port}`);
+    });
+
 });
 
-server.listen(port, hostName, function() {
-    console.log(`Now listening to port: ${port}`);
-});
+
+
